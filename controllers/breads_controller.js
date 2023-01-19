5e249d7f54408acac8aff4a6169e517e7eb63b7f
James Aules <james_aules@icloud.com>
2023-01-19 00:33:09 -0500

    Models

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,12 +1,17 @@
 const express = require("express")
+const bread = require("../models/bread.js")
 const breads = express.Router()
+const Bread = require("../models/bread.js")
 
 //INDEX
 breads.get("/", (req, res) => {
-    res.send("This is the index at /Breads ")
-
+    res.send(Bread)
 })
 
+//SHOW
+breads.get("/:arrayIndex", (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+})
 
 
 
