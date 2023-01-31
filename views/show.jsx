59ed0bd06f40a90fcded8e622be9112d50a0da03
James Aules <james_aules@icloud.com>
2023-01-30 19:25:24 -0500

    MongoDB edit & update

--- views/show.jsx
+++ views/show.jsx
@@ -2,7 +2,7 @@ const React = require("react")
 const bread = require("../models/bread")
 const Default = require("./layouts/Default")
 
-function Show({ bread, index }) {
+function Show({ bread}) {
     // make sure we are geting data in the terminal 
     console.log(bread.name)
     return (
@@ -26,11 +26,11 @@ function Show({ bread, index }) {
                 <a href="/breads" > Go Home</a>
             </li>
 
-            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+            <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
                 <input  type='submit' value="DELETE" />
             </form>
 
-            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
 
 
 
